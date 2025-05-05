bbc2fc666416333f7ff31e1a10af5658ea4c42bb
向着悠远的苍穹 <kdjlyy@qq.com>
2025-05-06 00:01:02 +0800

    add pretty output

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain_core.messages import AIMessage, HumanMessage
 from chains.models import load_vector_store
 from graph.graph import create_graph, stream_graph_updates, GraphState
 from utils.common import *
+from utils import pretty
 
 def main():
     # langchain.debug = True  # 启用langchain调试模式，可以获得如完整提示词等信息
@@ -34,20 +35,21 @@ def main():
             continue
         if user_input.lower() in ["e", "exit", 'q', "quit"]:
             break
-        state["messages"] = [HumanMessage(user_input + f"\n当前日期是: {get_current_time()}")]
+        state["messages"] = [HumanMessage(user_input)]
         # 流式获取AI的回复
         for answer in stream_graph_updates(graph, state, config):
             print(answer, end="")
         print()
 
     # 打印对话历史
-    print("\nHistory: ")
+    pretty.ALogger("[main]").title("History")
     for message in graph.get_state(config).values["messages"]:
         if isinstance(message, AIMessage):
             prefix = "AI"
         else:
             prefix = "User"
-        print(f"{prefix}: {message.content}")
+        pretty.ALogger("[main]").title(prefix)
+        print(f"{message.content}")
 
 if __name__ == "__main__":
     main()
